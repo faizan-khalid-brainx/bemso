37d4d1503c02eed1aa408f8729c0f7977a37289e
FaizanKhalid <faizan.khalid@brainxtech.com>
2021-07-01 19:06:07 +0500

    added migration and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create(['name'=>'Jhon Doe']);
+        \App\Models\Question::factory(3)->create();
+        \App\Models\Answer::factory(12)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::post('login','');
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('test',function (){
+        dd('test');
+    });
 });
