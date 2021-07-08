30f08646685281110542ede63159daf918697ada
FaizanKhalid <faizan.khalid@brainxtech.com>
2021-07-08 15:57:07 +0500

    added api call

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,15 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 
 class QuestionController extends Controller
 {
 
     public function display()
     {
-        //question, intermediary_table, users
-        $question = Question::all();
+        $questions = Question::with('user')->withCount('answers','user_votes')->get()
+            ->map(function ($question) {
+            // create object and extract necessary attributes
+            $returnable = Arr::only($question->getAttributes(),
+                ['id', 'title', 'content', 'created_at','answers_count','user_votes_count']);
+            // formatting date
+            $returnable['created_at'] = Carbon::parse($returnable['created_at'])->format('jS M Y');
+            $returnable['user'] = (object)Arr::only($question->getRelation('user')
+                ->getAttributes(), ['id', 'name', 'email']);
+            return (object)$returnable;
+        });
+        return response(json_encode(['questions' => $questions]),200);
     }
 
     public function store(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login'])
+Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login'])
     ->name('login');
 
-Route::get('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])
+Route::get('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])
     ->name('auth.logout');
 
-
-Route::get('/show-question',[App\Http\Controllers\QuestionController::class,'display'])
+Route::get('show-question',[App\Http\Controllers\QuestionController::class,'display'])
     ->name('question.display');
 
 Route::middleware(['auth:sanctum'])->group(function () {
