75f4d0165f1956ee79fe5ee1e617dcdc883fb440
FaizanKhalid <faizan.khalid@brainxtech.com>
2021-07-13 14:28:18 +0500

    code cleaning

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,7 +6,6 @@ use App\Models\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
 
 class QuestionController extends Controller
 {
@@ -17,9 +16,11 @@ class QuestionController extends Controller
             ->select(['id', 'title', 'content', 'created_at','user_id'])
             ->withCount('answers', 'user_votes')->get()
             ->map(function ($question) {
+                // removing foreign key
                 unset($question['user_id']);
                 // formatting date
                 $question['created_at'] = Carbon::parse($question['created_at'])->format('jS M Y');
+                // adding user object
                 $question['user'] = (object)$question->getRelation('user')->get();
                 return (object)$question;
             });
@@ -28,18 +29,18 @@ class QuestionController extends Controller
 
     public function index(Request $request)
     {
-        $queryResult = Question::where('id', $request->id)->with('user', 'answers', 'user_votes')->get()->first();
+        $queryResult = Question::where('id', $request->id)
+            ->with('user:id,name,email', 'user_votes')->get()->first();
         // extract attributes for question
         $question = Arr::only($queryResult
             ->getAttributes(), ['id', 'title', 'content', 'created_at']);
         // formatting date attribute
         $question['created_at'] = Carbon::parse($question['created_at'])->format('jS M Y');
         // extract user attributes for question
-        $question['user'] = (object)Arr::only($queryResult->getRelation('user')
-            ->getAttributes(), ['id', 'name', 'email']);
+        $question['user'] = (object)$queryResult->getRelation('user');
         // votes extracting question votes
         $isVoted = -1;
-        $vote = $queryResult->getRelations()['user_votes']->map(function ($user) use (&$isVoted) {
+        $vote = $queryResult->getRelation('user_votes')->map(function ($user) use (&$isVoted) {
             if ($user->getOriginal()['id'] == auth()->id()) {
                 $isVoted = $user->getOriginal()['pivot_vote'];
             }
@@ -60,9 +61,10 @@ class QuestionController extends Controller
             $Voted = -1;
             $returnable = Arr::only($answer->getAttributes(), ['id', 'content', 'created_at']);
             $returnable['created_at'] = Carbon::parse($returnable['created_at'])->format('jS M Y');
-            $returnable['user'] = (object)Arr::only($answer->user()->get()->first()
-                ->getAttributes(), ['id', 'name', 'email']);
-            $vote = $answer->getRelations()['user_votes']->map(function ($user) use (&$Voted) {
+            $returnable['user'] = (object)$answer->user()
+                ->select(['id', 'name', 'email'])
+                ->get()->first();
+            $vote = $answer->getRelation('user_votes')->map(function ($user) use (&$Voted) {
                 if ($user->getOriginal()['id'] == auth()->id()) {
                     $Voted = $user->getOriginal()['pivot_vote'];
                 }
